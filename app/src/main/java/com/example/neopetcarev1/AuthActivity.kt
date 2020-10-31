c409b8e56f0b5c6b2e6dfe09db5a9abf81bf36e5
u20181a686 (Tiglla Arrascue, Bruno) <u20181a686@upc.edu.pe>
2020-10-30 21:00:01 -0500

    Initial commit

---
+++ app/src/main/java/com/example/neopetcarev1/AuthActivity.kt
@@ -0,0 +1,16 @@
+package com.example.neopetcarev1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/neopetcarev1/HomeActiviy.kt
@@ -0,0 +1,11 @@
+ package com.example.neopetcarev1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActiviy : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_activiy)
+    }
+}
