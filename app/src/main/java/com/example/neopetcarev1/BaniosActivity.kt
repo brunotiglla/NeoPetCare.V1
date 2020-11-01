ef843fac0561327db7e76719f6c71c89312e0f70
u20181a686 (Tiglla Arrascue, Bruno) <u20181a686@upc.edu.pe>
2020-11-01 01:43:47 -0500

    implementacion de validaciones

--- app/src/main/java/com/example/neopetcarev1/BaniosActivity.kt
+++ app/src/main/java/com/example/neopetcarev1/BaniosActivity.kt
@@ -1,5 +1,6 @@
 package com.example.neopetcarev1
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
@@ -8,6 +9,11 @@ class BaniosActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_banios)
+
+        // Nuevas Funciones
+
+        FuncionesBasicas()
+
     }
 
     fun FuncionesBasicas(){
@@ -17,16 +23,20 @@ class BaniosActivity : AppCompatActivity() {
         val listaNew = findViewById<ListView>(R.id.ListaBanios)
 
 
-        btnListas.setOnClickListener {
+
+      //  val productos = Producto("brayan", "20/10/2027", "se baño en la mañana")
+      //  val ListaProductos = listOf<Producto>(productos)
+       // val adapter = ProductoAdapter(this, ListaProductos)
+
+       // listaNew.adapter = adapter
 
 
 
 
-
-
-
-
-        }
+       /* btnListas.setOnClickListener {
+            val intent = Intent(this, BaniosActivity::class.java)
+             startActivity(intent)
+       }*/
 
 
 
--- app/src/main/java/com/example/neopetcarev1/RegistroActiviy.kt
+++ app/src/main/java/com/example/neopetcarev1/RegistroActiviy.kt
@@ -1,4 +1,4 @@
- package com.example.neopetcarev1
+package com.example.neopetcarev1
 
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
